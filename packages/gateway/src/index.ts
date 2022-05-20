2a05ecf0a1c2ae6588056cc6931980e317dd71cc
codyborn <codyborn@outlook.com>
2022-05-20 17:07:19 -0400

    WIP

--- packages/gateway/src/index.ts
+++ packages/gateway/src/index.ts
@@ -1,24 +1,19 @@
 import { makeApp } from './server';
 import { Command } from 'commander';
-import { readFileSync } from 'fs';
 import { ethers } from 'ethers';
 import { JSONDatabase } from './json';
+require('dotenv').config()
+
 const program = new Command();
 program
-  .requiredOption(
-    '-k --private-key <key>',
-    'Private key to sign responses with. Prefix with @ to read from a file'
-  )
   .requiredOption('-d --data <file>', 'JSON file to read data from')
   .option('-t --ttl <number>', 'TTL for signatures', '300')
   .option('-p --port <number>', 'Port number to serve on', '8080');
 program.parse(process.argv);
 const options = program.opts();
-let privateKey = options.privateKey;
-if (privateKey.startsWith('@')) {
-  privateKey = ethers.utils.arrayify(
-    readFileSync(privateKey.slice(1), { encoding: 'utf-8' })
-  );
+let privateKey = process.env.PRIVATE_KEY;
+if (!privateKey) {
+  throw new Error("Missing expected private key env var");
 }
 const address = ethers.utils.computeAddress(privateKey);
 const signer = new ethers.utils.SigningKey(privateKey);
